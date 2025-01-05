4a6b5033c6c3d9c7870ac67c29b5f4cc31c2cf22
BestGaner68 <ayhan_roj@live.nl>
2025-01-05 20:24:36 +0100

    voertuigmigration en nieuwe endpoints

--- api/Models/Voertuig.cs
+++ api/Models/Voertuig.cs
@@ -10,12 +10,12 @@ namespace api.Models
     public class Voertuig
     {
         [Key]
-        public string VoertuigId { get; set; }  
+        public int VoertuigId { get; set; }  
         public string Merk { get; set; } = string.Empty;
         public string Kenteken { get; set; } =string.Empty;
-        public string kleur { get; set; }  =string.Empty;
-        public string type {  get; set; } =string.Empty;
+        public string Kleur { get; set; }  =string.Empty;
+        public string Type {  get; set; } =string.Empty;
         public int AanschafJaar { get; set; }
-        public bool Beschikbaar {get; set;}   
+        public string Soort {get; set;} = string.Empty;   
     }
 }
