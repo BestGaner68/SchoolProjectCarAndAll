9d7fe52a2e1837830129af59bd3725de1f2c4ec7
BestGaner68 <ayhan_roj@live.nl>
2025-01-26 18:55:00 +0100

    asd

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+511c49dcd0795556a2332a03f3431f6fcedab60e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d2675091362a397002faaba035d0bfa9cf834ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
