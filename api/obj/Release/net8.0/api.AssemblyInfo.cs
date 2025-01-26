258ac79c63fa78406ca5c68bfa6ce69b77547c9f
pjx19 <pjx1572@gmail.com>
2025-01-26 18:56:08 +0100

    .

--- api/obj/Release/net8.0/api.AssemblyInfo.cs
+++ api/obj/Release/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ab42bb6a9ec4d96ba73dc9aa290f1c5f9a4072")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b399c5d472bcf9b2b0c588a78e70e9ddd59e34")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
